1cf5fbb37fc11deabdee614e79af196c4a46fc1e
Matias Arenas <matias@groupon.com>
2015-09-21 22:50:38 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Matias Arenas Sepulveda on 9/21/15.
+//  Copyright (c) 2015 Matias Arenas Sepulveda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculator
+//
+//  Created by Matias Arenas Sepulveda on 9/21/15.
+//  Copyright (c) 2015 Matias Arenas Sepulveda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
